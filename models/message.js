02d1546860bc6ce755378088c7c044d62a1b5236
training <satishanil97@gmail.com>
2017-05-12 16:35:35 +0530

    connecting users with messages in the backend

--- models/message.js
+++ models/message.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
+var User = require('./user');
 
 var schema = new Schema({
   content: {type: String, required: true},
   user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+schema.post('remove', function(err, message) {  //mongoose provides this functionality to take an action based on an event in the database--here post => after
+  User.findById(message.user, function(err, user) {
+    user.messages.pull(message);
+    user.save();
+  });
 });
 
 module.exports = mongoose.model('Message',schema);
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Message = require('../models/message');
+var jwt = require('jsonwebtoken');
+var User= require('../models/user');
 
 router.get('/', function (req, res, next) {
   Message.find()
@@ -19,12 +21,25 @@ router.get('/', function (req, res, next) {
          });
 });
 
-router.post('/', function (req, res, next) {
-  var message = new Message({
-    content: req.body.content
-  });
+router.use('/', function(req, res, next) {  //use() ensures that for every route request, it invokes this function after get() defined above -- this function is to check if a useris authenticated before letting him do any actions
+  jwt.verify(req.query.token, 'secret',function (err, decoded) {
+    if(err) {
+      return res.status(500).json({
+        title: 'Not Authenticated',
+        error: err
+      });
+    }
 
-  message.save(function(err,result) {
+    next();   //proceed to the next mathcing request -- this is to let it continue to it's intended request if Authenticated
+  });
+});
+
+router.post('/', function (req, res, next) {
+  var decoded = jwt.decode(req.query.token); //decode() is used to decode a token...this function does'nt check for validity of token...but here we already checked validity earlier---
+  //if validity is not checked , then we must use verify() itself as that also returns decoded object
+
+  //now decoded contains the token
+  User.findById(decoded.user._id, function(err, user) {
     if(err) {
       return res.status(500).json({
         title: 'An Error Occured',
@@ -32,11 +47,28 @@ router.post('/', function (req, res, next) {
       });
     }
 
-    res.status(201).json({
-      message: 'Message Saved',
-      obj: result
+    var message = new Message({
+      content: req.body.content,
+      user: user  //linking message to the user
+    });
+
+    message.save(function(err, result) {
+      if(err) {
+        return res.status(500).json({
+          title: 'An Error Occured',
+          error: err
+        });
+      }
+
+      user.messages.push(result); //add this message to the message list of current user
+      user.save();
+      res.status(201).json({
+        message: 'Message Saved',
+        obj: result
+      });
     });
   });
+
 });
 
 router.patch('/:id', function (req, res, next) {    //PATCH - http request used for modifying an existing resource -- PUT request is used to entirely replace a resource
@@ -95,7 +127,7 @@ router.delete('/:id', function (req, res, next) {    //DELETE - http request use
           error: err
         });
       }
-
+      //removing this message from the list of messages of current user is done by the mongoose middleware that is defined in models/messages.js
       res.status(201).json({
         message: 'Message Deleted',
         obj: result
