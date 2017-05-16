8173e7b5b83a785fae909285e69c63409a9f4464
training <satishanil97@gmail.com>
2017-05-16 16:04:38 +0530

    finally ready for deployment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messageRoutes = require('./routes/messages');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('https://angular-messenger.herokuapp.com/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
